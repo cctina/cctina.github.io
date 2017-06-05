cb7d718abd10ffaeff6da08d22647699c0d35a66
Yu-Chen Hsu <every2ever@gmail.com>
2017-06-05 16:29:22 -0700

    [work] fix data attribute name from 'image' to 'images'

--- js/templates.js
+++ js/templates.js
@@ -23,8 +23,8 @@ templates['work-detailitem'] = template({"1":function(container,depth0,helpers,p
     + "</span>\r\n		</li>\r\n		<li class=\"flat-list-item workdetail-info\">\r\n			<span class=\"workdetail-info-title\">Platform</span> <span class=\"workdetail-info-value\">"
     + alias4(((helper = (helper = helpers.platform || (depth0 != null ? depth0.platform : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"platform","hash":{},"data":data}) : helper)))
     + "</span>\r\n		</li>\r\n	</ul>\r\n</div>\r\n<div class=\"workdetail-content__block\">\r\n";
-  stack1 = ((helper = (helper = helpers.image || (depth0 != null ? depth0.image : depth0)) != null ? helper : alias2),(options={"name":"image","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data}),(typeof helper === alias3 ? helper.call(alias1,options) : helper));
-  if (!helpers.image) { stack1 = helpers.blockHelperMissing.call(depth0,stack1,options)}
+  stack1 = ((helper = (helper = helpers.images || (depth0 != null ? depth0.images : depth0)) != null ? helper : alias2),(options={"name":"images","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data}),(typeof helper === alias3 ? helper.call(alias1,options) : helper));
+  if (!helpers.images) { stack1 = helpers.blockHelperMissing.call(depth0,stack1,options)}
   if (stack1 != null) { buffer += stack1; }
   return buffer + "</div>	\r\n<div class=\"workdetail-content__block\">\r\n	<span class=\"workdetail-info-title\">Keywords</span> \r\n	<ul class=\"flat-list\">\r\n"
     + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.keywords : depth0),{"name":"each","hash":{},"fn":container.program(3, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
@@ -42,9 +42,9 @@ templates['work-listitem'] = template({"1":function(container,depth0,helpers,par
   return "<div class=\"work__item\" data-id=\""
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
     + "\">\r\n	<div class=\"work__img\">\r\n		<a class=\"work-link\" href=\"\"><img class=\"work-img\" src=\""
-    + alias4(alias5(((stack1 = ((stack1 = (depth0 != null ? depth0.image : depth0)) != null ? stack1["0"] : stack1)) != null ? stack1.path : stack1), depth0))
+    + alias4(alias5(((stack1 = ((stack1 = (depth0 != null ? depth0.images : depth0)) != null ? stack1["0"] : stack1)) != null ? stack1.path : stack1), depth0))
     + "\" alt=\""
-    + alias4(alias5(((stack1 = ((stack1 = (depth0 != null ? depth0.image : depth0)) != null ? stack1["0"] : stack1)) != null ? stack1.alt : stack1), depth0))
+    + alias4(alias5(((stack1 = ((stack1 = (depth0 != null ? depth0.images : depth0)) != null ? stack1["0"] : stack1)) != null ? stack1.alt : stack1), depth0))
     + "\"/></a>\r\n		<a class=\"work-cover work-link work-cover-hidden\" href=\"\">\r\n			<div class=\"work-cover-content\">\r\n				<!-- <p><i class=\"fa fa-eye\"></i></p> -->\r\n				<p>"
     + alias4(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper)))
     + "</p>\r\n			</div>\r\n		</a>\r\n	</div>\r\n	<div class=\"work__text\">\r\n		<a class=\"work-link\" href=\"\"><p class='work__text-title'>"
@@ -53,7 +53,7 @@ templates['work-listitem'] = template({"1":function(container,depth0,helpers,par
     + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.keywords : depth0),{"name":"each","hash":{},"fn":container.program(2, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + "		</ul>\r\n	</div>\r\n</div>\r\n";
 },"2":function(container,depth0,helpers,partials,data) {
-    return "			<li class=\"work__tag work__tag-'-basic\">"
+    return "			<li class=\"work__tag work__tag--basic\">"
     + container.escapeExpression(container.lambda(depth0, depth0))
     + "</li>\r\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
